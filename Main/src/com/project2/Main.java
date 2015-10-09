e3d68b60504ac22cc740c3d439bf3bd2cd319cb8
Sorgrum <marcelogheiler@gmail.com>
2015-10-09 15:18:25 -0400

    Initial creation

---
+++ Main/src/com/project2/Main.java
@@ -0,0 +1,11 @@
+package com.project2;
+
+/**
+ * Created by mgheiler on 10/9/15.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
+
